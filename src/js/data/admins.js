8b622f279119b6e4406db71ff29ed4448968dde2
Proshanto Lal <proshantolal3@gmail.com>
2022-09-07 18:22:35 +0600

    Adds email property to user and admin objects

--- src/js/data/admins.js
+++ src/js/data/admins.js
@@ -1,11 +1,13 @@
 const admin1 = {
     username: 'admin',
     password: 'admin', 
+    email: 'admin@bs23.com',
 }
 
 const admin2 = {
     username: 'proshanto',
     password: 'fym',
+    email: 'proshanto@bs23.com',
 }
 
 const admins = [admin1,admin2];
--- src/js/data/users.js
+++ src/js/data/users.js
@@ -1,11 +1,13 @@
 const user1 = {
     username: 'imran',
     password: '1234', 
+    email: 'MdImranMir@gmail.com',
 }
 
 const user2 = {
     username: 'rafi',
-    password: '1234',
+    password: '5678',
+    email: 'rafi69@gmail.com'
 }
 
 const users = [user1,user2];
