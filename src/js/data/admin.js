4c2cdf0481fc9ca2ca67c6d98d1cfb37819e1f16
Proshanto Lal <proshantolal3@gmail.com>
2022-09-07 15:17:07 +0600

    Adds name tag for sign in and sign up form in index.html and adds data folder which contains admin credentials

---
+++ src/js/data/admin.js
@@ -0,0 +1,11 @@
+const admin1 = {
+    username: 'admin',
+    password: 'admin', 
+}
+
+const admin2 = {
+    username: 'proshanto',
+    password: 'fym',
+}
+
+export const admins = [admin1,admin2]; 
