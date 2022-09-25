4e9f1bf654c5e48364478bb7f7a65155723f7b1f
Proshanto <proshantolal3@gmail.com>
2022-09-25 10:43:55 +0600

    Eliminates unused code

--- src/js/helpers/hidingHelpers.js
+++ src/js/helpers/hidingHelpers.js
@@ -1,14 +1,3 @@
-import { MSG_LOAD_TIME } from '../config';
-import loginView from '../views/loginView';
-
-export const hideModal = function () {
-  setTimeout(loginView.renderMessage.bind(loginView), MSG_LOAD_TIME * 1000);
-  setTimeout(() => {
-    loginView.renderMessage();
-    setTimeout(loginView.toogleWindow.bind(loginView), MSG_LOAD_TIME * 220);
-  }, MSG_LOAD_TIME * 1000);
-};
-
 export const hideBookmark = function () {
   parentEl = document.querySelector('.btn--bookmark');
   if (!parentEl.classList.contains('hidden')) parentEl.classList.add('hidden');
